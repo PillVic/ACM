8503f5088abe33c9c8495bd50dcb069a3b8b9c87
PillVic <1093332977@qq.com>
2017-12-13 17:05:06 +0800

    done

--- 1233/1233/源.cpp
+++ 1233/1233/源.cpp
@@ -2,10 +2,13 @@
 #include<stdlib.h>
 #include<algorithm>
 
-#define MIN(A,B) A<B?A:B
-#define MAX(A,B) A>B?A:B
-
 using namespace std;
+
+/*
+计算通路的最小开销即求最小生成树
+使用Kruskal算法，生成最小生成树，
+同时将接受的边的开销累加，即为答案
+*/
 
 typedef struct {
 	int town1;
@@ -13,47 +16,86 @@ typedef struct {
 	int distance;
 }road;
 
-typedef int SetType;
-typedef SetType *Set;
-bool IsNearer(void *a, void*b) {
-	return (*(road*)a).distance < (*(road*)b).distance;
+typedef int SetElement;
+typedef SetElement *Set;
+
+inline SetElement min(SetElement e1, SetElement e2) { return e1 < e2 ? e1 : e2; }
+inline SetElement max(SetElement e1, SetElement e2) { return e1 > e2 ? e1 : e2; }
+
+inline bool IsNearer(road a, road b) {
+	return a.distance < b.distance;
 }
 
-Set BuildSet(int Numeber);
-SetType Find(Set S, SetType e);
-void Merge(Set S, SetType e1, SetType e2);
-void DestroySet(Set S);
-static int length;
+static int SetNumber;
+
+inline Set BuildSet(int Numeber);
+inline SetElement Find(Set S, SetElement e);
+void Merge(Set S, SetElement e1, SetElement e2);
+inline void DestroySet(Set S);
+
 
 int main() {
-	int towns;
-	while (cin >> towns) {
-		if (towns == 0) {
+	int townNum;
+	while (cin >> townNum) {
+		if (townNum == 0) {
 			break;
 		}
-		Set cities = BuildSet(towns);
-		int lines = towns * (towns - 1) / 2;
-		road *roads = (road*)malloc(lines * sizeof(road));
-		for (int i = 0; i < lines; i++) {
+		if (townNum == 1) {
+			cout << 0 << endl;
+			continue;
+		}
+		//input the data
+		Set towns = BuildSet(townNum);
+		int roadNum = townNum * (townNum - 1) / 2;
+		road *roads = (road*)malloc(roadNum * sizeof(road));
+		for (int i = 0; i < roadNum; i++) {
 			cin >> roads[i].town1 >> roads[i].town2 >> roads[i].distance;
 		}
-		sort(roads, roads + lines, IsNearer);
-
+		//sort data
+		sort(roads, roads + roadNum, IsNearer);
+		//Kruskal algrithm
+		double cost = roads[0].distance;
+		Merge(towns, roads[0].town1, roads[0].town2);
+		for (int index = 1; index < roadNum; index++) {
+			int town1 = roads[index].town1;
+			int town2 = roads[index].town2;
+			if(Find(towns,town1)!=Find(towns,town2)){
+				Merge(towns,town1, town2);
+				cost += roads[index].distance;
+			}
+		}
+		cout << cost << endl;
+		DestroySet(towns);
+		free(roads);
 	}
 	return 0;
 }
 
 Set BuildSet(int Number) {
-	Set S = (SetType*)malloc(Number * sizeof(SetType));
-	length = Number;
+	Set S = (SetElement*)malloc(Number * sizeof(SetElement));
+	SetNumber = Number;
 	for (int i = 0; i < Number; i++) {
 		S[i] = i + 1;
 	}
 	return S;
 }
-SetType Find(Set S, SetType e) {
+SetElement Find(Set S, SetElement e) {
 	return S[e - 1];
 }
-void Merge(Set S, SetType e1, SetType e2) {
-
+void Merge(Set S, SetElement e1, SetElement e2) {
+	if (Find(S, e1) == Find(S, e2)) {
+		return;
+	}
+	else {
+		SetElement t = min(Find(S, e1), Find(S, e2));
+		SetElement m = max(Find(S, e1), Find(S, e2));
+		for (int index = 0; index < SetNumber; index++) {
+			if (S[index] == t) {
+				S[index] = m;
+			}
+		}
+	}
+}
+void DestroySet(Set S) {
+	free(S);
 }
