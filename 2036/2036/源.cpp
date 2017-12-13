a015e5416a19ecf63a8bcb2ee49f4eb44be0a5fa
PillVic <1093332977@qq.com>
2017-12-13 10:36:37 +0800

    done

---
+++ 2036/2036/源.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<iomanip>
+#include<stdlib.h>
+
+using namespace std;
+
+typedef struct {
+	int x;
+	int y;
+}node;
+
+inline double crossProduct(node a, node b) {
+	return a.x* b.y - a.y*b.x;
+}
+
+int main() {
+	int nodeNumber;
+	while (cin >> nodeNumber) {
+		if (nodeNumber == 0) {
+			break;
+		}
+		//input data
+		node * nodes = (node*)malloc(nodeNumber * sizeof(node));
+		for (int i = 0; i < nodeNumber; i++) {
+			cin >> nodes[i].x >> nodes[i].y;
+		}
+		double sumArea = 0;
+		for (int index = 0; (index+1) < nodeNumber; index++) {
+			sumArea += crossProduct(nodes[index], nodes[index + 1]);
+		}
+		sumArea += crossProduct(nodes[nodeNumber - 1], nodes[0]);
+		sumArea /= 2;
+		cout << setprecision(1) << std::fixed << sumArea << endl;
+		free(nodes);
+	}
+	return 0;
+}
