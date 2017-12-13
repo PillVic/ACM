f905c9a3bf651eafae9903657b232d00c6f460e6
PillVic <1093332977@qq.com>
2017-12-13 13:01:09 +0800

    done

--- 1213/1213/源.cpp
+++ 1213/1213/源.cpp
@@ -1,70 +1,79 @@
 #include<iostream>
-#include<stdlib.h>
 #include<algorithm>
-
-#define MIN(x,y) x<y?x:y
 using namespace std;
 
-//Set implement
-typedef int *Set;
-typedef int SetType;
-typedef int ElementType;
 
-Set BuildSet(int Number);
-void SetUnion(Set s, SetType e1, SetType e2);
-SetType Find(Set S, SetType e);
-static int length;
-void Destroy(Set S);
+
+//实现并查集
+typedef int *Set;
+typedef int setElement;
+typedef int Position;
+
+static int ElementNumber;
+
+inline setElement max(setElement a, setElement b) { return a > b ? a : b; }
+inline setElement min(setElement a, setElement b) { return a < b ? a : b; }
+
+inline Set buildSet(int Number);
+inline Position Find(Set S, setElement e1);
+void Merge(Set S, setElement e1, setElement e2);
+inline void Destroy(Set S);
 
 int main() {
 	int Cases;
-	while (cin >> Cases) {
-		for (int i = 0; i < Cases; i++) {
-			int Number;
-			int lines;
-			cin >> Number >> lines;
-			Set	S = BuildSet(Number);
-			for (int line = 0; line < lines; line++) {
-				SetType e1, e2;
-				cin >> e1 >> e2;
-				SetUnion(S, e1, e2);
-			}
-			sort(S, S + length);
-			SetType start = S[0];
-			int num = 1;
-			for (int i = 0; i < length; i++) {
-				if (S[i] != start) {
-					num += 1;
-					start = S[i];
-				}
-			}
-			cout << num<< endl;
-			Destroy(S);
+	cin >> Cases;
+	while (Cases--) {
+		int Number;
+		cin >> Number;
+		Set S = buildSet(Number);
+		int lines;
+		cin >> lines;
+		for (int line = 0; line < lines; line++) {
+			setElement e1, e2;
+			cin >> e1 >> e2;
+			Merge(S, e1, e2);
 		}
+		//Calculate the Set Number
+		sort(S, S + ElementNumber);
+		int SetNumber = 1;
+		setElement t = S[0];
+		for (int index = 1; index < ElementNumber; index++) {
+			if (t != S[index]) {
+				SetNumber += 1;
+				t = S[index];
+			}
+		}
+		cout << SetNumber << endl;
+		Destroy(S);
 	}
 	return 0;
 }
-Set BuildSet(int Number) {
-	Set s = (SetType*)malloc(Number * sizeof(SetType));
-	length = Number;
-	for (int i = 0; i < length; i++) {
-		s[i] = i + 1;
+Set buildSet(int Number) {
+	Set S = new setElement[Number];
+	ElementNumber = Number;
+	for (int i = 0; i < Number; i++) {
+		S[i] = i + 1;
 	}
-	return s;
+	return S;
 }
-void SetUnion(Set s, SetType e1, SetType e2) {
-	SetType newS = MIN(s[e1-1], s[e2-1]);
-	SetType oldS = s[e1-1] + s[e2-1] - newS;
-	for (int i = 0; i < length; i++) {
-		if (s[i] == oldS) {
-			s[i] = newS;
+Position Find(Set S, setElement e) {
+	return S[e-1];
+}
+
+void Merge(Set S, setElement e1, setElement e2) {
+	if (Find(S, e1) == Find(S, e2)) {
+		return;
+	}
+	else {
+		setElement t = max(Find(S,e1), Find(S,e2));
+		setElement m = min(Find(S, e1), Find(S, e2));
+		for (int index = 0; index < ElementNumber; index++) {
+			if (S[index] == t) {
+				S[index] = m;
+			}
 		}
 	}
 }
-SetType Find(Set s, SetType e) {
-	return s[e - 1];
-}
 void Destroy(Set S) {
-	free(S);
-	S = NULL;
+	delete S;
 }
