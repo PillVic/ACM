6eb9e8c319c02e0bbbd32e7d06120a4dafa87e70
PillVic <1093332977@qq.com>
2018-01-10 09:14:02 +0800

    done

--- 2092/2092/源.cpp
+++ 2092/2092/源.cpp
@@ -1,12 +1,25 @@
-#include<iostream>
-#include<algorithm>
+#include<stdio.h>
+#include<math.h>
 
-using  namespace std;
+//#define scanf_s(A,B,C) scanf(A,B,C)
 
 int main() {
-	double m, p;
-	while (cin >> m >> p) {
-
+	int sum, product;
+	while (scanf_s("%d %d", &sum, &product)!=EOF) {
+		if (sum == product && sum == 0) {
+			break;
+		}
+		else {
+			int minus =int(sqrt(double(sum * sum - product * 4)));
+			int x = (sum + minus) / 2;
+			int y = sum - x;
+			if (product == x * y&&minus == abs(x - y)) {
+				printf("Yes\n");
+			}
+			else {
+				printf("No\n");
+			}
+		}
 	}
 	return 0;
 }
