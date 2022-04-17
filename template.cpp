329c078f30ca1596e38926d30e1ec1f040a39e10
neovic <16205111@hdu.edu.cn>
2022-04-17 12:15:20 +0800

    优化流程

---
+++ template.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+/**
+ * oj cpp模板
+ * */
+using namespace std;
+
+const char endline = '\n';
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    return 0;
+}
