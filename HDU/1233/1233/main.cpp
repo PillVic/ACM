f192bd92215999492351bfe0437a34dab1fbc007
neovic <16205111@hdu.edu.cn>
2023-11-02 10:08:15 +0800

    1232:最小生成树板子题 prim算法实现

--- HDU/1233/1233/main.cpp
+++ HDU/1233/1233/main.cpp
@@ -1,75 +1,56 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-//DisJoint SET 
-typedef vector<int> Set;
-typedef int SetType;
-typedef int ElementType;
+/*
+ * 最小生成树板子题
+ * 此处使用Prim算法实现
+ * */
 
-void Initialize(Set &S, int length);
-void SetUnion(Set &S, SetType Root1, SetType Root2);
-SetType Find(ElementType x, Set &S);
-/////////////////////////
+const int MAX_TOWN = 101;
+const int inf = 0x3fffffff;
+int G[MAX_TOWN][MAX_TOWN];
 
-typedef struct{
-    int x, y;
-    int cost;
-}road;
-
-bool judge(road r1, road r2){
-    return r1.cost < r2.cost;
+int prime(const int& townNum){
+    int totalCost = 0;
+    vector<int> costs = vector<int> (townNum, inf);
+    vector<bool> visited = vector<bool> (townNum, false);
+    //任意一个点作为起点都可以, 直接约定0为起点
+    costs[0] = 0;
+    for(int i=0;i<townNum; i++){
+        int minCost = inf;
+        int town = -1;
+        for(int j=0; j< townNum;j++){
+            if(minCost > costs[j] and (not visited[j])){
+                minCost = costs[j];
+                town = j;
+            }
+        }
+        visited[town] = true;
+        totalCost += minCost;
+        for(int targetTown = 0; targetTown<townNum;targetTown++){
+            if(not visited[targetTown]){
+                costs[targetTown] = min(costs[targetTown], G[town][targetTown]);
+            }
+        }
+    }
+    return totalCost;
 }
-int main(){
+int main(void){
     ios::sync_with_stdio(false);
-    int cityNum, roadNum;
-    while(cin>>cityNum){
-	if(cityNum == 0){
-	    break;
-	}
-	int roadNum = (cityNum*(cityNum-1))/2;
-	vector<road> r;
-	for(int i=0;i<roadNum;i++){
-	    road t;
-	    cin>>t.x>>t.y>>t.cost;
-	    t.x -=1;
-	    t.y -=1;
-	    r.push_back(t);
-	}
-	sort(r.begin(), r.end(), judge);
-	Set S;
-	Initialize(S, cityNum);
-	int result = 0;
-	for(const auto &t:r){
-	    if(Find(t.x, S)!=Find(t.y, S)){
-		result +=t.cost;
-		SetUnion(S, t.x, t.y);
-#ifdef LOCAL
-		for(const auto i:S){
-		    cout<<i<<" ";
-		}
-		cout<<endl;
-		cout<<"BUIILD ROAD:"<<t.x<<" "<<t.y<<" "<<t.cost<<endl;
-#endif
-	    }
-	}
-	cout<<result<<endl;
+    int townNum;
+    while(cin >>  townNum){
+        if(townNum == 0){
+            break;
+        }
+        int roadNum = (townNum * (townNum - 1))/2;
+        for(int i=0; i<roadNum; i++){
+            int x, y, cost;
+            cin >> x >> y >> cost;
+            x -= 1;
+            y -= 1;
+            G[x][y] = G[y][x] = cost;
+        }
+        cout << prime(townNum) << endl;
     }
     return 0;
-}
-void Initialize(Set &S, int length){
-    for(int i=0;i<length;i++){
-	S.push_back(i);
-    }
-}
-void SetUnion(Set &S, SetType Root1, SetType Root2){
-    int i = min(S[Root1], S[Root2]);
-    int j = S[Root1]+S[Root2] - i;
-    for(auto &k:S){
-	if(k == j){
-	    k = i;
-	}
-    }
-}
-SetType Find(ElementType x, Set &S){
-    return S[x];
 }
